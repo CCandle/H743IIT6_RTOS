b997909c4bf87c2f4ea7c2326382043667c95c80
Carlos QU <Carlos_QU@163.com>
2025-11-17 11:09:05 +0800

    feat: display enable

--- App/Tasks/LCDTask.hpp
+++ App/Tasks/LCDTask.hpp
@@ -29,10 +29,10 @@ public:
 
   void Load_Drow_Dialog(void) // 从左到右  从上到下
   {
-    // LT768_DrawSquare_Fill(0, 0, 480, 270, color65k_white);
+    LT768_DrawSquare_Fill(0, 0, 480, 270, color65k_white);
     Active_Window_XY(0, 30);
     Active_Window_WH(4000, 1200);
-    // LT768_DrawSquare_Fill(120, 50, 250, 200, color65k_red);
+    LT768_DrawSquare_Fill(120, 50, 250, 200, color65k_red);
   }
 
   void Run() {
@@ -42,27 +42,34 @@ public:
     vTaskDelay(pdMS_TO_TICKS(300));
 
     Display_ON();
-    // StartUp_picture();
-    Select_Main_Window_24bpp();
+    StartUp_picture();
 
     vTaskDelay(pdMS_TO_TICKS(300));
 
     LT768_PWM1_Init(1, 0, 50, 100, 100);
 
     // Load_Drow_Dialog();
+    delay_ms(1000);
+    LT768_Select_Internal_Font_Init(24, 2, 2, 1, 1);
 
     while (1) {
       // Fill the screen with red, green and blue with rectangular function
       LT768_DrawSquare_Fill(0, 0, LCD_XSIZE_TFT, LCD_YSIZE_TFT, color16M_red);
       delay_ms(1000);
 
-      LT768_DrawSquare_Fill(0, 0, LCD_XSIZE_TFT, LCD_YSIZE_TFT, color16M_green);
+      // LT768_DrawSquare_Fill(0, 0, LCD_XSIZE_TFT, LCD_YSIZE_TFT, color16M_green);
+      // delay_ms(1000);
+
+      // LT768_DrawSquare_Fill(0, 0, LCD_XSIZE_TFT, LCD_YSIZE_TFT, color16M_blue);
+      // delay_ms(1000);
+
+      LT768_DrawCircleSquare(200, 30, 500, 20, 10, 10, color16M_blue);
       delay_ms(1000);
 
-      LT768_DrawSquare_Fill(0, 0, LCD_XSIZE_TFT, LCD_YSIZE_TFT, color16M_blue);
-      delay_ms(1000);
+      // LT768_DrawLine(20,20,50,50,color16M_yellow);
+      // delay_ms(1000);
 
-      LT768_DrawCircleSquare(200, 30, 500, 20, 10, 10, 0x00f0f0f0);
+      LT768_Print_Internal_Font_String(400, 200, color16M_black, color16M_white, (char*)"Hello LT768!");
       delay_ms(1000);
     }
   }
